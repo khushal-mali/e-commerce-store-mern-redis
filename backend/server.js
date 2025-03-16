f74333431b406cb0e4e455ba5ce448153b47dfe9
Khushal Mali <khushalmali8448@gmail.com>
2025-03-16 23:09:44 +0530

    entry route of the backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
-import { express } from "express";
+import { configDotenv } from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
 
-const PORT = process.env.PORT;
+// route imports
+import authRoutes from "./routes/auth.routes.js";
+import productRoutes from "./routes/products.routes.js";
 
+// DB import
+import { connectDB } from "./lib/db.js";
+
+configDotenv({});
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`App running on server port::${PORT}...`);
+app.use(express.json()); // Allows you to parse body of the request
+app.use(cookieParser());
+
+// manage routes
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server running on server http://localhost:${PORT}`);
+  connectDB();
 });
