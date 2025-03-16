bd009c6c08db14dd5b5ce61837bf00b9fa51497e
Khushal Mali <khushalmali8448@gmail.com>
2025-03-16 23:20:42 +0530

    db setup

---
+++ backend/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log(`MONGODB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to mongodb: `, error);
+    process.exit(1);
+  }
+};
