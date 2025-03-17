e7a4a2e6ef992cb2116f9263a34d40127482704d
Khushal Mali <khushalmali8448@gmail.com>
2025-03-17 15:13:12 +0530

    routes updated

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+  deleteProduct,
+  createProduct,
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/product.controller.js";
 import { adminRoute, protectedRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectedRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.post("/", protectedRoute, adminRoute, createProduct);
+router.delete("/:id", protectedRoute, adminRoute, deleteProduct);
 
 export default router;
