ddf3cb05d5731f32ae7e332f2f2ce92f1c71dae8
Khushal Mali <khushalmali8448@gmail.com>
2025-03-19 13:56:12 +0530

    cart queries and mutations

---
+++ backend/controllers/cart.controller.js
@@ -0,0 +1,96 @@
+import Product from "../models/product.model.js";
+
+export const addToCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const user = req.user;
+
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      user.cartItems.push(productId);
+    }
+
+    await user.save();
+    return res.status(201).json(user.cartItems);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error while adding product to cart", error: error.message });
+  }
+};
+
+export const removeAllFromCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const user = req.user;
+
+    if (!productId) {
+      user.cartItems = [];
+    } else {
+      user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+    }
+
+    await user.save();
+    return res.status(201).json(user.cartItems);
+  } catch (error) {
+    console.log(`[fileName: 'cart.controller', Line Number: '36']`, error.message);
+    return res
+      .status(500)
+      .json({ message: "Error while adding product to cart", error: error.message });
+  }
+};
+
+export const updateQuantity = async (req, res) => {
+  try {
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+
+    if (existingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+        await user.save();
+        return res.status(201).json(user.cartItems);
+      }
+
+      existingItem.quantity = quantity;
+      await user.save();
+      return res.status(200).json(user.cartItems);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (error) {
+    console.log(`[fileName: 'cart.controller', Line Number: '62']`, error.message);
+    return res.status(500).json({
+      message: "Error while updating quantity of the product",
+      error: error.message,
+    });
+  }
+};
+
+export const getCartProducts = async (req, res) => {
+  try {
+    const user = req.user;
+
+    const products = await Product.find({ _id: { $in: req.user.cartItems } });
+
+    console.log(products);
+
+    const cartItems = products.map((product) => {
+      const item = req.user.cartItems.find((cartItem) => cartItem.id === product.id);
+      return { ...product.toJSON(), quantity: item.quantity };
+    });
+
+    console.log(cartItems);
+
+    return res.status(200).json(cartItems);
+  } catch (error) {
+    console.log(`[fileName: 'cart.controller', Line Number: '91']`, error.message);
+    return res
+      .status(500)
+      .json({ message: "Error while fetching cart products", error: error.message });
+  }
+};
