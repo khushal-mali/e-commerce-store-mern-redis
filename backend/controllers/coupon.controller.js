b3004df214f3e9edad428641d2b8bba6d8182aa4
Khushal Mali <khushalmali8448@gmail.com>
2025-03-19 14:29:06 +0530

    added coupon validate function query

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -6,8 +6,38 @@ export const getCoupon = async (req, res) => {
 
     return res.status(200).json(coupon || null);
   } catch (error) {
+    console.log(`[fileName: 'coupon.controller', Line Number: '9']`, error.message);
     return res.status(500).json({
       message: "Unable to get Coupon for user",
+      error: error.message,
+    });
+  }
+};
+
+export const validateCoupon = async (req, res) => {
+  try {
+    const { code } = req.body;
+
+    const coupon = await Coupon.findOne({ code, userId: req.user._id, isActive: true });
+    if (!coupon) {
+      return res.status(404).json({ message: "Coupon not found!" });
+    }
+
+    if (coupon.expirationDate < new Date()) {
+      coupon.isActive = false;
+      await coupon.save();
+      return res.status(401).json({ message: "Coupon is Expired!" });
+    }
+
+    return res.status(200).json({
+      message: "Coupon is valid",
+      code: coupon.code,
+      discountPercentage: coupon.discountPercentage,
+    });
+  } catch (error) {
+    console.log(`[fileName: 'coupon.controller', Line Number: '38']`, error.message);
+    return res.status(500).json({
+      message: "Unable to validate coupon",
       error: error.message,
     });
   }
