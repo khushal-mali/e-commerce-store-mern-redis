a439fb8e71ef8063fc416e228b5ed2fbae2a4ce2
Khushal Mali <khushalmali8448@gmail.com>
2025-03-23 01:03:37 +0530

    product global store

---
+++ frontend/src/stores/useProductStore.js
@@ -0,0 +1,96 @@
+import { create } from "zustand";
+import axios from "../lib/axios";
+import { toast } from "react-hot-toast";
+
+export const useProductStore = create((set, get) => ({
+  products: [],
+  loading: false,
+
+  setProducts: (products) => set({ products }),
+
+  createProduct: async (productData) => {
+    set({ loading: true });
+
+    try {
+      const { data } = await axios.post("/products", productData);
+
+      set((prevState) => ({
+        products: [...prevState.products, data.product],
+        loading: false,
+      }));
+      toast.success(data.message);
+      return data;
+    } catch (error) {
+      console.log(
+        `[fileName: 'useProductStore', Line Number: '25']`,
+        error.message,
+      );
+      set({ loading: false });
+      return toast.error(error.response.data.message);
+    }
+  },
+
+  fetchAllProducts: async () => {
+    set({ loading: true });
+
+    try {
+      const { data } = await axios.get("/products");
+      set({ products: data.products, loading: false });
+    } catch (error) {
+      console.log(
+        `[fileName: 'useProductStore', Line Number: '40']`,
+        error.message,
+      );
+      set({ loading: false });
+      return toast.error(
+        error.response.data.message || "Failed to fetch products.",
+      );
+    }
+  },
+
+  deleteProduct: async (productId) => {
+    set({ loading: true });
+
+    try {
+      const { data } = await axios.delete(`/products/${productId}`);
+      set((prevState) => ({
+        products: prevState.products.filter(
+          (product) => product._id !== productId,
+        ),
+        loading: false,
+      }));
+      toast.success(data.message);
+    } catch (error) {
+      console.log(
+        `[fileName: 'useProductStore', Line Number: '62']`,
+        error.message,
+      );
+      set({ loading: false });
+      return toast.error(error.response.data.message);
+    }
+  },
+
+  toggleFeaturedProduct: async (productId) => {
+    set({ loading: true });
+
+    try {
+      const { data } = await axios.patch(`/products/${productId}`);
+      set((prevState) => ({
+        products: prevState.products.map((product) =>
+          product._id === productId
+            ? { ...product, isFeatured: data.product.isFeatured }
+            : product,
+        ),
+        loading: false,
+      }));
+      toast.success(data.message);
+    } catch (error) {
+      console.log(
+        `[fileName: 'useProductStore', Line Number: '88']`,
+        error.message,
+      );
+      set({ loading: false });
+      return toast.error(error.response.data.message || "Failed to toggle.");
+    }
+  },
+}));
