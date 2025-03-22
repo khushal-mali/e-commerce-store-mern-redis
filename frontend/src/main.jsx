d11ed945ddf69e3393394476ff3f88126950143d
Khushal Mali <khushalmali8448@gmail.com>
2025-03-22 11:56:41 +0530

    toaster config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
@@ -7,7 +8,8 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <Toaster position="bottom-right" reverseOrder={true} />
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
