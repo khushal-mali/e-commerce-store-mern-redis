d9247c66c757e4874b9cadc5fdc5a10937d76ce2
Khushal Mali <khushalmali8448@gmail.com>
2025-03-25 15:09:19 +0530

    bug fix

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -6,7 +6,7 @@ const productSchema = new mongoose.Schema(
     description: { type: String, required: [true, "Description is required."] },
     price: { type: Number, required: true, min: 0 },
     image: { type: String, required: [true, "Image is required."] },
-    type: { type: String, required: [true, "Type is required."] },
+    category: { type: String, required: [true, "Type is required."] },
     isFeatured: { type: Boolean, default: false },
   },
   { timestamps: true }
