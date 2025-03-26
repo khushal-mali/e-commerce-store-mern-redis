a7ccff976218690e09d3dcd1ea19cc3e66e64ff3
Khushal Mali <khushalmali8448@gmail.com>
2025-03-26 23:08:47 +0530

    update

--- backend/routes/payment.routes.js
+++ backend/routes/payment.routes.js
@@ -7,7 +7,7 @@ import { protectedRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("create-checkout-session", protectedRoute, createCheckoutSession);
-router.post("checkout-success", protectedRoute, checkoutSuccess);
+router.post("/create-checkout-session", protectedRoute, createCheckoutSession);
+router.post("/checkout-success", protectedRoute, checkoutSuccess);
 
 export default router;
