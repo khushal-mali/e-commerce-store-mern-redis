dba410443006820d443b023a300c5fbfddfd13f3
Khushal Mali <khushalmali8448@gmail.com>
2025-03-28 10:38:26 +0530

    ui fix

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -30,22 +30,28 @@ const ProductCard = ({ product }) => {
       </div>
 
       <div className="mt-4 px-5 pb-5">
-        <h5 className="text-xl font-semibold tracking-tight text-white">
-          {product.name}
+        <h5 className="mb-2 line-clamp-3 text-xl font-semibold tracking-tight text-white">
+          {product?.name}
         </h5>
+
+        <h5 className="line-clamp-2 tracking-tight text-white">
+          {product?.description}
+        </h5>
+
         <div className="mt-2 mb-5 flex items-center justify-between">
           <p>
             <span className="text-3xl font-bold text-emerald-400">
-              ${product.price}
+              ${product?.price}
             </span>
           </p>
         </div>
+
         <button
           className="flex cursor-pointer items-center justify-center rounded-lg bg-emerald-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-emerald-700 focus:ring-4 focus:ring-emerald-300 focus:outline-none"
           onClick={handleAddToCart}
         >
           <ShoppingCart size={22} className="mr-2" />
-          Add to cart
+          Add to Cart
         </button>
       </div>
     </div>
